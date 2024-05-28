d84329efa8af8e2363b3fef11130afd7c6f6750d
Lakshya Dhawan <lakshyad1902@gmail.com>
2024-05-29 00:07:27 +0530

    added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechyStar</h1>
+        <p>@all rights reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://www.linkedin.com/in/lakshya-dhawan19" target="blank">
+            Linkedin
+          </a>
+          <a href="https://leetcode.com/u/lakshyad1902" target="blank">
+            Leetcode
+          </a>
+          <a href="https://github.com/yolakshya19" target="blank">
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
