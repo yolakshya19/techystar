7e41181c1938b3d6b55e9290576a4d44a3b65023
Lakshya Dhawan <lakshyad1902@gmail.com>
2024-05-28 23:12:28 +0530

    added home3

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -22,6 +22,25 @@ const Home = () => {
           </p>
         </div>
       </div>
+
+      <div className="home3">
+        <div>
+          <h1>Who are we?</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio
+            modi fugit autem ex nihil beatae ipsam ipsa cum. Sunt eos impedit
+            ratione, iure doloribus amet alias repellendus unde error non
+            deserunt at est, voluptates voluptas fugiat rem natus aperiam nihil
+            laudantium odit iste. Quasi provident accusantium cum. Porro
+            cupiditate doloribus vero praesentium amet in laboriosam sunt
+            voluptate hic consequuntur, non unde sint voluptates a! Possimus
+            voluptatibus labore ipsum, eveniet magnam in temporibus culpa fuga
+            iure, harum porro vitae earum velit impedit atque eaque omnis eos
+            aspernatur id beatae laborum at consequatur dolorum molestias. Ullam
+            delectus amet suscipit placeat. Officiis, aspernatur.
+          </p>
+        </div>
+      </div>
     </>
   );
 };
