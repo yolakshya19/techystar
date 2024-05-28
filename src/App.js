ef53a698bd1d8ebf0f252d08c74ea31612ab7d5c
Lakshya Dhawan <lakshyad1902@gmail.com>
2024-05-28 18:37:47 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Header from "./components/header";
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route />
       </Routes>
